828cd98b66f957b2e6d83f1f5e720d59a423b1b2
Ravindra <mravindra0122@gmail.com>
2025-11-30 08:01:09 +0530

    added test file

---
+++ .idea/Test-File.py
@@ -0,0 +1,36 @@
+import os
+import urllib.request
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+urls_and_paths = {
+    "https://raw.githubusercontent.com/saiadityaus1/SparkCore1/master/test.txt": os.path.join(data_dir, "test.txt"),
+}
+
+for url, path in urls_and_paths.items():
+    urllib.request.urlretrieve(url, path)
+
+
+# ======================================================================================
+
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+from pyspark.sql import SparkSession
+import sys
+import os
+from collections import namedtuple
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+os.environ['JAVA_HOME'] = r'C:\Program Files\Microsoft\jdk-21.0.8.9-hotspot'
+
+conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost")
+sc = SparkContext(conf=conf)
+
+spark = SparkSession.builder.getOrCreate()
+
+spark.read.format("csv").load("data/test.txt").toDF("Success").show(20,False)
